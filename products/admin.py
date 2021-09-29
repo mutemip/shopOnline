4402db447d44827262995862e1c1d7ea0326c23d
mutemip <paulmutemi2030@gmail.com>
2021-09-29 23:04:05 +0300

    add media files configuration

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Products, Manufacturer
+from .models import Products, Manufacturer
 # Register your models here.
 
 admin.site.register(Products)
